1353c4b2b0635c47437ad075d52382deeee270d0
Ajay Gandhi <ajay.gandhi@meraki.net>
2021-05-03 15:15:41 -0400

    Convert heic images to jpg

--- server.js
+++ server.js
@@ -9,6 +9,21 @@ const config = require("./config.json");
 const app = express();
 const port = 8080;
 
+const writeFile = async (filename, data) => {
+  const tempPath = path.join(__dirname, "temp", filename);
+  const permPath = path.join(config.filesDir, filename);
+
+  fs.writeFile(tempPath, outputBuffer, (err) => {
+    exec("mv " + tempPath + " " + permPath, (err, stdout, stderr) => {
+      if (err) {
+        console.log("Error moving to volume");
+        console.log(err);
+        console.log(stderr);
+      }
+    });
+  });
+};
+
 app.use(fileUpload({
   limits: { fileSize: 50 * 1024 * 1024 },
 }));
@@ -22,17 +37,20 @@ app.get("/", (req, res) => {
 app.post("/upload", (req, res) => {
   Object.keys(req.files).forEach((fileKey) => {
     const file = req.files[fileKey];
-    const filename = `${file.md5}${path.extname(file.name)}`;
-    const tempPath = path.join(__dirname, "temp", filename);
-    const permPath = path.join(config.filesDir, filename);
-    fs.writeFileSync(tempPath, file.data);
-    exec("mv " + tempPath + " " + permPath, (err, stdout, stderr) => {
-      if (err) {
-        console.log("Error moving to volume");
-        console.log(err);
-        console.log(stderr);
-      }
-    });
+    const ext = path.extname(file.name);
+    const newFilename = `${file.md5}${ext}`;
+
+    if (ext.toLowerCase() === ".heic") {
+      convert({
+        buffer: file.data,
+        format: "JPEG",
+        quality: 1,
+      }).then((output) => {
+        writeFile(newFilename, outputBuffer);
+      });
+    } else {
+      writeFile(newFilename);
+    }
   });
   res.send({ success: true });
 });
